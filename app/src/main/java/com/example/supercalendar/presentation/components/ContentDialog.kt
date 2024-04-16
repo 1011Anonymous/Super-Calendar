3e4922580e1ffb2473f5bfdfa21e1fc4a786b07d
1011Anonymous <96935167+1011Anonymous@users.noreply.github.com>
2024-04-16 16:40:18 +0800

    添加了显示周数的功能

--- app/src/main/java/com/example/supercalendar/presentation/components/ContentDialog.kt
+++ app/src/main/java/com/example/supercalendar/presentation/components/ContentDialog.kt
@@ -1,15 +1,11 @@
 package com.example.supercalendar.presentation.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
